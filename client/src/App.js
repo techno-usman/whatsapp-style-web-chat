4a03c312f0a628df0c95b37407427c00722555df
Muhammad Usman Ch <developermolvi@gmail.com>
2022-09-10 20:41:59 +0500

    google login added

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import Messenger from './components/Messenger';
 
 function App() {
+
+  const clientId = "462416745771-ls6pbhgr2i7tnaj8uc5takv3607jhrs0.apps.googleusercontent.com";
+
   return (
-    <Messenger />
+    <GoogleOAuthProvider clientId={clientId}>
+      <Messenger />
+    </GoogleOAuthProvider>
+
   );
 }
 
--- client/src/components/account/LoginDialog.jsx
+++ client/src/components/account/LoginDialog.jsx
@@ -1,27 +1,49 @@
-import { Dialog, Box, Typography, List, ListItem } from "@mui/material";
+import { Dialog, Box, Typography, List, ListItem, styled } from "@mui/material";
+import { GoogleLogin } from "@react-oauth/google";
+import jwt_decode from "jwt-decode";
+
+const Component = styled(Box)`
+    display:flex;
+    margin:56px;
+`
+
+const Title = styled(Typography)`
+    font-size:24px;
+`
 
 const dialogStyle = {
     height: "100vh",
     marginTop: "12%",
     width: "60vw",
+    maxWidth: "100vw",
     boxShadow: "none",
+};
 
+const onLoginSuccess = (response) => {
+    const decode = jwt_decode(response.credential);
+    console.log(decode);
+};
+
+const onLoginError = (response) => {
+    console.log(response);
 };
 
 const LoginDialog = () => {
     return (
         <Dialog open={true} PaperProps={{ sx: dialogStyle }}>
-            <Box>
+            <Component>
                 <Box>
-                    <Typography>To use WhatsApp on your computer:</Typography>
+                    <Title>To use Chat on your computer:</Title>
                     <List>
-                        <ListItem>Open WhatsApp on your phone</ListItem>
-                        <ListItem>Tap Menu or Settings and select Linked Devices</ListItem>
-                        <ListItem>Point your phone to this screen to capture the code</ListItem>
+                        <ListItem>1. Open Chat on your phone</ListItem>
+                        <ListItem>2. Tap Menu or Settings and select Linked Devices</ListItem>
+                        <ListItem>3. Point your phone to this screen to capture the code</ListItem>
                     </List>
                 </Box>
-                <Box></Box>
-            </Box>
+                <Box>
+                    <GoogleLogin onSuccess={onLoginSuccess} onError={onLoginError} />
+                </Box>
+            </Component>
         </Dialog>
     );
 };
